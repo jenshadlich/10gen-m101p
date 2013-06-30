fcdb48e1001a3a1a108864d920213d16a88cddba
jenshadlich <jenshadlich@googlemail.com>
2013-06-30 21:10:44 +0200

    homework done

--- week02/hw2-3/userDAO.py
+++ week02/hw2-3/userDAO.py
@@ -55,6 +55,7 @@ class UserDAO:
         try:
             # XXX HW 2.3 Students Work Here
             # you will need to retrieve right document from the users collection.
+            user = self.users.find_one({ '_id': username })
             print "This space intentionally left blank."
         except:
             print "Unable to query database for user"
@@ -85,7 +86,7 @@ class UserDAO:
             # XXX HW 2.3 Students work here
             # You need to insert the user into the users collection.
             # Don't over think this one, it's a straight forward insert.
-
+            self.users.insert(user)
             print "This space intentionally left blank."
 
         except pymongo.errors.OperationFailure:
