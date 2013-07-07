64c97d5f8eba8f7f525302537af7bdc343b91d2d
jenshadlich <jenshadlich@googlemail.com>
2013-07-07 15:39:42 +0200

    homework 3 done

--- week03/hw3-2and3-3/blogPostDAO.py
+++ week03/hw3-2and3-3/blogPostDAO.py
@@ -55,6 +55,7 @@ class BlogPostDAO:
         # now insert the post
         try:
             # XXX HW 3.2 Work Here to insert the post
+            self.posts.insert(post)
             print "Inserting the post"
         except:
             print "Error inserting post"
@@ -68,6 +69,7 @@ class BlogPostDAO:
         cursor = []         # Placeholder so blog compiles before you make your changes
 
         # XXX HW 3.2 Work here to get the posts
+        cursor = self.posts.find().sort('date',-1).limit(num_posts)
 
         l = []
 
@@ -92,6 +94,7 @@ class BlogPostDAO:
 
         post = None
         # XXX Work here to retrieve the specified post
+        post = self.posts.find_one({'permalink' : permalink})
 
         if post is not None:
             # fix up date
@@ -110,7 +113,7 @@ class BlogPostDAO:
         try:
             last_error = {'n':-1}           # this is here so the code runs before you fix the next line
             # XXX HW 3.3 Work here to add the comment to the designated post
-
+            self.posts.update({'permalink': permalink}, {'$push' : {'comments': comment}})
 
             return last_error['n']          # return the number of documents updated
 
